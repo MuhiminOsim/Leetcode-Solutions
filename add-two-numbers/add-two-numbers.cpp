0cd0e230243572a620e21b7cd6168a080879b89e
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-01-02 21:22:02 +0600

    Update add-two-numbers.cpp

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -12,46 +12,21 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode* head = NULL;
-        ListNode* prev = NULL;
+        ListNode head;
+        ListNode* prev = &head;
         
-        while(l1 != NULL && l2 != NULL) {
-            int sum = l1->val + l2->val + carry;
-            ListNode* newNode = new ListNode(sum % 10);
-            if(head == NULL) head = newNode;
-            if(prev != NULL) prev->next = newNode;
-            carry = sum / 10;
-            prev = newNode;
-            l1 = l1->next;
-            l2 = l2->next;
+        while(l1 || l2 || carry) {
+            
+            carry = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
+            
+            prev->next = new ListNode(carry % 10);
+            prev = prev->next;
+            carry = carry > 9;
         }
         
-        while(l1 != NULL) {
-            int sum = l1->val + carry;
-            ListNode* newNode = new ListNode(sum % 10);
-            if(head == NULL) head = newNode;
-            if(prev != NULL) prev->next = newNode;
-            carry = sum / 10;
-            prev = newNode;
-            l1 = l1->next;
-        }
-        
-        while(l2 != NULL) {
-            int sum = l2->val + carry;
-            ListNode* newNode = new ListNode(sum % 10);
-            if(head == NULL) head = newNode;
-            if(prev != NULL) prev->next = newNode;
-            carry = sum / 10;
-            prev = newNode;
-            l2 = l2->next;
-        }
-        
-        if(carry) {
-            ListNode* newNode = new ListNode(carry);
-            if(head == NULL) head = newNode;
-            if(prev != NULL) prev->next = newNode;
-        }
-        
-        return head;
+        return head.next;
     }
 };
