601774c179e04aeae6a041ed4630b7bfe9248864
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-01-29 17:24:22 +0600

    Time: 16 ms (39.44%), Space: 6.9 MB (68.17%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     
-    bool check(string& s, int st, int en, char ch) {
-        if(ch == '.') return true;
-        for(int i = st; i <= en; i++) {
-            if(s[i] != ch) return false;
-        }
-        return true;
-    }
-    
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
         vector<vector<bool>> dp(n + 1, vector<bool> (m + 1, false));
@@ -24,11 +16,7 @@ public:
                 }
                 dp[i][j + 1] = dp[i][j + 1] || dp[i][j - 1];
                 dp[i + 1][j + 1] = dp[i + 1][j + 1] || dp[i + 1][j - 1];
-                for(int k = 0; k <= i; k++) {
-                    if(check(s, k, i, p[j - 1])) {
-                        dp[i + 1][j + 1] = dp[i + 1][j + 1] || dp[k][j - 1];
-                    }
-                }
+                dp[i + 1][j + 1] = dp[i + 1][j + 1] || (dp[i][j + 1] && (p[j -1] == '.' || s[i] == p[j - 1]));
             }
         }
         
