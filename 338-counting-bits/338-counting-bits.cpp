6a9e82326ef327b7286cf6e2f4181416504ae599
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-03-01 22:37:41 +0600

    Time: 7 ms (62.32%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,12 +3,9 @@ public:
     vector<int> countBits(int n) {
         vector<int> countOne(n + 1, 0);
         
-        for(int i = 0; i <= n; i++) {
-            int num = i;
-            while(num) {
-                if(num & 1) countOne[i]++;
-                num /= 2;
-            }
+        for(int i = 1; i <= n; i++) {
+            if(i & 1) countOne[i] = countOne[i >> 1] + 1;
+            else countOne[i] = countOne[i >> 1];
         }
         
         return countOne;
