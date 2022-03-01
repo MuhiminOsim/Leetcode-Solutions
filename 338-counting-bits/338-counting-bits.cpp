85870225f10ce8465cd19a9b3817146b1dfa4a71
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-03-01 22:40:56 +0600

    Time: 3 ms (95.58%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> countOne(n + 1, 0);
         
         for(int i = 1; i <= n; i++) {
-            countOne[i] = countOne[i >> 1] + (i & 1);
+            countOne[i] = countOne[(i >> 1)] + (i & 1);
         }
         
         return countOne;
