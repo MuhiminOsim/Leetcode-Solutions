8de628f08bf9812935827da3b72ace45dedeb2f9
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-07-07 16:14:13 +0600

    Time: 901 ms (5.02%), Space: 23.2 MB (27.56%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int size1 = s1.size();
+        int size2 = s2.size();
+        int size3 = s3.size(); 
+        vector<vector<vector<bool>>> memo(size3 + 1, vector<vector<bool>> (size1 + 1, vector<bool> (size2 + 1, false)));
+        
+        memo[0][0][0] = true;
+        
+        for(int i = 0; i < size3; i++) {
+            for(int j = -1; j < size1; j++) {
+                for(int k = -1; k < size2; k++) {
+                    if(j >= 0 && s3[i] == s1[j]) {
+                        memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j][k + 1];
+                    }
+                    if(k >= 0 && s3[i] == s2[k]) {
+                        memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j + 1][k];
+                    }
+                }
+            }
+        }
+        
+        return memo[size3][size1][size2];
+    }
+};
