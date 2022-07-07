87bbe2196cdb4817cbd5dfad19d76c7290a92b9c
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-07-07 16:21:38 +0600

    Time: 213 ms (5.02%), Space: 23.3 MB (27.56%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -10,13 +10,14 @@ public:
         
         for(int i = 0; i < size3; i++) {
             for(int j = -1; j < size1; j++) {
-                for(int k = -1; k < size2; k++) {
-                    if(j >= 0 && s3[i] == s1[j]) {
-                        memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j][k + 1];
-                    }
-                    if(k >= 0 && s3[i] == s2[k]) {
-                        memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j + 1][k];
-                    }
+                if(j > i) break;
+                int k = i - j - 1;
+                if(k >= size2) continue;
+                if(j >= 0 && s3[i] == s1[j]) {
+                    memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j][k + 1];
+                }
+                if(k >= 0 && s3[i] == s2[k]) {
+                    memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j + 1][k];
                 }
             }
         }
