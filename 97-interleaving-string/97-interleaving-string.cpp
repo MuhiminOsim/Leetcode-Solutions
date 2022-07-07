76c2a194736c68f1a0341d2e33f333115f032000
Muhiminul Islam Osim <32521249+MuhiminOsim@users.noreply.github.com>
2022-07-07 16:28:49 +0600

    Time: 4 ms (73.94%), Space: 6.7 MB (66.40%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,10 +3,12 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int size1 = s1.size();
         int size2 = s2.size();
-        int size3 = s3.size(); 
-        vector<vector<vector<bool>>> memo(size3 + 1, vector<vector<bool>> (size1 + 1, vector<bool> (size2 + 1, false)));
+        int size3 = s3.size();
+        if(size3 != (size1 + size2)) return false;
         
-        memo[0][0][0] = true;
+        vector<vector<bool>> memo(size1 + 1, vector<bool> (size2 + 1, false));
+        
+        memo[0][0] = true;
         
         for(int i = 0; i < size3; i++) {
             for(int j = -1; j < size1; j++) {
@@ -14,14 +16,14 @@ public:
                 int k = i - j - 1;
                 if(k >= size2) continue;
                 if(j >= 0 && s3[i] == s1[j]) {
-                    memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j][k + 1];
+                    memo[j + 1][k + 1] = memo[j + 1][k + 1] || memo[j][k + 1];
                 }
                 if(k >= 0 && s3[i] == s2[k]) {
-                    memo[i + 1][j + 1][k + 1] = memo[i + 1][j + 1][k + 1] || memo[i][j + 1][k];
+                    memo[j + 1][k + 1] = memo[j + 1][k + 1] || memo[j + 1][k];
                 }
             }
         }
         
-        return memo[size3][size1][size2];
+        return memo[size1][size2];
     }
 };
